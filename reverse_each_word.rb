9b76d8fbbeee895ada5d91ff1d1f0be3d85bad0d
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-20 10:49:23 -0600

    finsihed lab

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,9 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = words.map(&:reverse)
+    reversed_words.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
